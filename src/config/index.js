77edadaf664ce048140054364b3c03c899b87918
Ismael Mauricio de Oliveira <oliveiraismael2@gmail.com>
2020-08-04 00:17:01 -0300

    Mais uma atualização na url do backend

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL_BACKEND_TOP = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'http://fe-flix.herokuapp.com';
+  : 'https://kelloflix.herokuapp.com';
 
 export default {
   URL_BACKEND_TOP,
